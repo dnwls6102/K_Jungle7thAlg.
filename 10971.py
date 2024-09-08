319a19117f7b805749a2b339c74dd3ead0efa6a7
dnwls6102 <dnwls6102@gmail.com>
2024-09-09 00:43:50 +0900

    10971 수정

--- 10971.py
+++ 10971.py
@@ -9,42 +9,25 @@ for i in range(n):
     temp = [int(x) for x in temp]
     arr.append(temp)
         
-def DFS(start, current, end, arr, cost, turn, flags):
+def DFS(start, current, arr, cost, flags):
     global min
-    if (turn == 1):
-        cost = arr[current][end]
-        #print(cost)
-        
-    else :
-        cost += arr[current][end]
-        #print(cost)
-        if (turn == 4):
+    
+    if (not False in flags):
+        if (arr[current][start] != 0):
+            cost += arr[current][start]
             if (cost < min):
                 min = cost
-            else:
-                return
     
-    turn += 1
-    
-    for i in range(len(arr[current])):
-        if (turn == 4):
-            flags[start] = True
-            DFS(start, end, start, arr, cost, turn ,flags)
-            flags[start] = False
-        
-        elif (flags[i] == False and i != start):
-            flags[i] = True
-            DFS(start, end, i, arr, cost, turn, flags)
-            flags[i] = False
+    else :
+        for i in range(n):
+            if (flags[i] == False and arr[current][i] != 0 and cost + arr[current][i] < min):
+                flags[i] = True
+                DFS(start, i, arr, cost + arr[current][i] , flags)
+                flags[i] = False
             
 for t in range(n):
-    if t < 3:
-        flags[t+1] = True
-        DFS(t, t, t + 1, arr, 0, 1, flags)
-        flags[t+1] = False
-    else :
-        flags[0] = True
-        DFS(t, t, 0, arr, 0, 1, flags)
-        flags[0] = False
+    flags[t] = True
+    DFS(t, t, arr, 0, flags)
+    flags[t] = False
     
 print(min)
